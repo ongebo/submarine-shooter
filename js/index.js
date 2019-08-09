ac544a37f7ba363a0e5bc1564c786cf1d025d7b2
ongebo <isaacongebo@gmail.com>
2019-08-09 14:43:59 +0300

    Refactor the buildWorld() function

--- js/index.js
+++ js/index.js
@@ -25,53 +25,14 @@ function init() {
 }
 
 function buildWorld() {
-    var subBody, subTop, subRotor;
-
-    // Create the submarine's body.
-    var subBodyGeometry = new THREE.SphereGeometry(10, 100, 100);
-    var subBodyMaterial = new THREE.MeshLambertMaterial({ color: 0x000000 });
-    subBody = new THREE.Mesh(subBodyGeometry, subBodyMaterial);
-    subBody.scale.x = 4;
-
-    // Create the submarine's top.
-    var subTopGeometry = new THREE.BoxGeometry(10, 5, 7);
-    var subTopMaterial = new THREE.MeshLambertMaterial({ color: 0x000000 });
-    subTop = new THREE.Mesh(subTopGeometry, subTopMaterial);
-    subTop.position.y = 10.5;
-
-    // Create a cylinder to construct the submarine's rotor.
-    var cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2, 10, 1, false);
-    var cylinderMaterial = new THREE.MeshLambertMaterial({ color: 0xe5f500 });
-    var cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-
-    // Create cylindrical parts for the submarine's rotor.
-    cylinder.scale.set(0.7, 4, 0.7);
-    var rotorShaft = cylinder.clone();
-
-    cylinder.scale.set(0.4, 2, 0.4);
-    cylinder.rotation.z = Math.PI / 2;
-    cylinder.position.y = -3;
-    var rotorWing1 = cylinder.clone();
-
-    cylinder.rotation.y = Math.PI / 3;
-    var rotorWing2 = cylinder.clone();
-
-    cylinder.rotation.y += Math.PI / 3;
-    var rotorWing3 = cylinder.clone();
-
-    // Create the submarine's rotor from its individual parts.
-    subRotor = new THREE.Object3D();
-    subRotor.add(rotorShaft);
-    subRotor.add(rotorWing1);
-    subRotor.add(rotorWing2);
-    subRotor.add(rotorWing3);
-    subRotor.rotation.z = Math.PI / 2;
-    subRotor.position.x = 43;
+    var subBody = createSubBody();
+    var subTower = createSubTower();
+    var subRotor = createSubRotor();
 
     // Create the submarine.
     var submarine = new THREE.Object3D();
     submarine.add(subBody);
-    submarine.add(subTop);
+    submarine.add(subTower);
     submarine.add(subRotor);
 
     scene.add(submarine);
@@ -80,6 +41,51 @@ function buildWorld() {
     var light = new THREE.DirectionalLight();
     light.position.set(0, 0, 100);
     scene.add(light);
+}
+
+function createSubBody() {
+    var subBodyGeometry = new THREE.SphereGeometry(10, 100, 100);
+    var subBodyMaterial = new THREE.MeshLambertMaterial({ color: 0x000000 });
+    var subBody = new THREE.Mesh(subBodyGeometry, subBodyMaterial);
+    subBody.scale.x = 4;
+    return subBody;
+}
+
+function createSubTower() {
+    var subTopGeometry = new THREE.BoxGeometry(10, 5, 7);
+    var subTopMaterial = new THREE.MeshLambertMaterial({ color: 0x000000 });
+    var subTower = new THREE.Mesh(subTopGeometry, subTopMaterial);
+    subTower.position.y = 10.5;
+    return subTower;
+}
+
+function createSubRotor() {
+    // Create a cylinder to construct the submarine's rotor.
+    var cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2, 10, 1, false);
+    var cylinderMaterial = new THREE.MeshLambertMaterial({ color: 0xe5f500 });
+    var cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+
+    // Create cylindrical parts for the submarine's rotor.
+    cylinder.scale.set(0.7, 4, 0.7);
+    var rotorShaft = cylinder.clone();
+    cylinder.scale.set(0.4, 2, 0.4);
+    cylinder.rotation.z = Math.PI / 2;
+    cylinder.position.y = -3;
+    var rotorWing1 = cylinder.clone();
+    cylinder.rotation.y = Math.PI / 3;
+    var rotorWing2 = cylinder.clone();
+    cylinder.rotation.y += Math.PI / 3;
+    var rotorWing3 = cylinder.clone();
+
+    // Create the submarine's rotor from its individual parts.
+    var subRotor = new THREE.Object3D();
+    subRotor.add(rotorShaft);
+    subRotor.add(rotorWing1);
+    subRotor.add(rotorWing2);
+    subRotor.add(rotorWing3);
+    subRotor.rotation.z = Math.PI / 2;
+    subRotor.position.x = 43;
+    return subRotor;
 }
 
 function animate() {
